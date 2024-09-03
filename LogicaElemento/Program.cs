94efc8138755a79af9bcd0abeeef8bd0a3ac3525
Savio Fagundes <akirasfm@gmail.com>
2024-09-03 19:18:10 -0300

    Adicionar arquivos de projeto.

---
+++ LogicaElemento/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LogicaElemento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Descubra a lógica e complete o próximo elemento:
+            
+            Console.Write("a) 1, 3, 5, 7, 9 (Logica: numero anterior acrescido de 2)\n" +
+                "b) 2, 4, 8, 16, 32, 64, 128 (Logica: numero anterior multiplicado por 2)\n" +
+                "c) 0, 1, 4, 9, 16, 25, 36, 49 (Logica: numero de unidade vezes ele mesmo)\r\n" +
+                "d) 4, 16, 36, 64, 100 (Logica: Numero é igual ao quadrado dos pares)\n" +
+                "e) 1, 1, 2, 3, 5, 8, 13 (Logica: fibonacci sequency)\r\n" +
+                "f) 2,10, 12, 16, 17, 18, 19, 200 (Logica: próximo numero começado com a letra D)\r\n");
+        }
+    }
+}
